54bf567ca8c047d4d6ede8559b034e6bc883a549
TUSHAR <165839694+tusharrohilla11@users.noreply.github.com>
2025-07-14 15:35:30 +0530

    Update page.jsx

--- app/dashboard/upgrade/page.jsx
+++ app/dashboard/upgrade/page.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Upgrade = () => {
   return (
     <>
-           <h2 className='font-medium text-lg mb-3'>Reach out to me , I'll promoted you as admin , So you can created a unlimted course with Ai</h2>
+           <h2 className='font-medium text-lg mb-3'></h2>
       <div className="relative w-full h-screen overflow-hidden">
         <iframe className='absolute inset-0 w-full h-full border-none overflow-hidden'
-        src="https://buymeacoffee.com/mrpankajpandey_"
+        src="https://tr-portfolio-wine.vercel.app/"
         
-        title="Buy Me a Coffee Profile"
+        title="TR Portfolio "
         />
         </div>
         </>
